7b9fb7d7bad44bd0005714e88d257d791c09b1df
thesharkza <thesharkza@gmail.com>
2026-05-08 08:53:33 +0700

    app.py

--- app.py
+++ app.py
@@ -13,10 +13,13 @@ import numpy as np
 import pandas as pd
 from supabase import create_client, Client
 
-# เชื่อมต่อกับ Supabase โดยใช้ Secrets
-url: str = st.secrets["SUPABASE_URL"]
-key: str = st.secrets["SUPABASE_KEY"]
-supabase: Client = create_client(url, key)
+@st.cache_resource
+def init_connection():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
+
+supabase: Client = init_connection()
 
 # --- CONFIG ---
 st.set_page_config(page_title="GEM System 10.0 (The Oracle)", layout="wide", initial_sidebar_state="expanded")
